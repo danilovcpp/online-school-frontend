71760561f31612829a35a74638f81c98f8584c20
Alexandr Tokarev <a.tokarev@dats.team>
2025-10-24 20:12:50 +0600

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import { PropsWithChildren } from "react";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Online school",
+  description: "Online school",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
