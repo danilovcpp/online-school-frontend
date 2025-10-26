713b64d828f3675dbb115facc78f1ef7dadda0e0
Alexandr Tokarev <a.tokarev@dats.team>
2025-10-26 22:35:38 +0600

    refactor: remove header

--- src/app/(portal)/trainers/guess-result/page.tsx
+++ src/app/(portal)/trainers/guess-result/page.tsx
@@ -1,18 +1,7 @@
 import { GuessResultPage } from '@/features/trainers/pages/guess-result/guess-result';
 
 const Page = () => {
-  return (
-    <div className="max-w-7xl mx-auto px-5 py-5">
-      <header className="text-center py-10">
-        <h1 className="text-5xl font-extrabold mb-3">Угадай результат</h1>
-        <h2 className="block text-lg font-light text-white/70 mt-3">Тренируйте память и ментальный счет</h2>
-      </header>
-
-      <main>
-        <GuessResultPage />
-      </main>
-    </div>
-  );
+  return <GuessResultPage />;
 };
 
 export default Page;
